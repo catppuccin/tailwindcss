a06fac46a69db867683cdef80f967ef5fb29cfbd
winston <hey@winston.sh>
2022-07-05 04:35:37 +0200

    chore: update github pages subdir

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/catppuccin-tailwindcss/',
-  plugins: [react()]
-})
+	base: "/tailwindcss/",
+	plugins: [react()],
+});
