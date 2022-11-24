bcafa545d3df0873d595ce4489233a43ff4a78d9
winston <hey@winston.sh>
2022-11-24 22:43:51 +0100

    feat: typescript rewrite

--- plugin.ts
+++ plugin.ts
@@ -1,9 +1,18 @@
-const plugin = require("tailwindcss/plugin");
-const ctp = require("@catppuccin/palette");
+// const plugin = require("tailwindcss/plugin");
+import * as plugin from "tailwindcss/plugin";
+import { variants } from "@catppuccin/palette";
+import { Config } from "tailwindcss";
+import { ThemeConfig } from "tailwindcss/types/config";
+import {
+  CatppuccinColour,
+  CatppuccinFlavour,
+  CatppuccinPluginOptions,
+  WithOpacityFn,
+} from "./plugin.d";
 
 // helper function for css values + opacity in tailwind < 3.1
-const withOpacity = (variableName) => {
-  return ({ opacityValue }) => {
+const withOpacity = (variableName: string) => {
+  return ({ opacityValue }: { opacityValue: number | undefined }) => {
     if (opacityValue !== undefined) {
       return `rgba(var(${variableName}), ${opacityValue})`;
     }
@@ -12,23 +21,24 @@ const withOpacity = (variableName) => {
 };
 
 // generate an object with the catppuccin palette
-const palette = {};
-Object.keys(ctp.variants).map((variant) => {
+const palette: Record<string, Record<string, string>> = {};
+Object.keys(variants).map((variant) => {
   // insert a key into the colours object, with an empty object
   palette[variant] = {};
   // for each colour...
-  Object.keys(ctp.variants[variant]).map((colour) => {
+  Object.keys(variants[variant as CatppuccinFlavour]).map((colour) => {
     // insert a key into the colours object
-    palette[variant][colour] = ctp.variants[variant][colour].hex;
+    palette[variant][colour] =
+      variants[variant as CatppuccinFlavour][colour as CatppuccinColour].hex;
   });
 });
 
 // two constants keeping track of the names of the variants & colours
-const variants = Object.keys(palette);
-const colours = Object.keys(palette[variants[0]]);
+const flavours = Object.keys(palette);
+const colours = Object.keys(palette[flavours[0]]);
 
 // converts '#000000' to '0, 0, 0' for the css variables
-const parseHexToRGB = (hex) => {
+const parseHexToRGB = (hex: string) => {
   // get the colour, drop the '#' if it's there
   const hexColour = hex.replace("#", "");
   // convert the colour to r g,b in base 10
@@ -39,9 +49,12 @@ const parseHexToRGB = (hex) => {
 };
 
 // generates the css variables, injected in the addBase() function
-const generateColorCss = (defaultFlavour = "", prefix = false) => {
-  const result = {};
-  variants.map((variant) => {
+const generateColorCss = (
+  defaultFlavour = "",
+  prefix: string | boolean = false
+) => {
+  const result: Record<string, Record<string, string>> = {};
+  flavours.map((variant) => {
     // if a prefix is defined, use e.g. '.ctp-mocha' instead of '.mocha'
     const className = prefix ? `.${prefix}-${variant}` : `.${variant}`;
 
@@ -60,8 +73,8 @@ const generateColorCss = (defaultFlavour = "", prefix = false) => {
 
 // generates the 'options' mapping in tailwind.config.js
 // this extends the theme & adds the names of the colours
-const generateOptions = (prefix = false) => {
-  const result = {};
+const generateOptions = (prefix: string | boolean = false) => {
+  const result: Record<string, WithOpacityFn> = {};
   colours.map((colour) => {
     const keyName = prefix ? `${prefix}-${colour}` : colour;
     // withOpacity is used to provide backward compatibility with Tailwind < 3.1
@@ -71,7 +84,7 @@ const generateOptions = (prefix = false) => {
 };
 
 // every colour key available in tailwindcss
-const colourConfigKeys = [
+const colourConfigKeys: Array<keyof ThemeConfig> = [
   "backgroundColor",
   "borderColor",
   "caretColor",
@@ -86,23 +99,26 @@ const colourConfigKeys = [
   "textColor",
 ];
 
-module.exports = plugin.withOptions(
-  (options) => {
+export default plugin.withOptions(
+  (options: CatppuccinPluginOptions) => {
     return ({ addBase }) => {
       addBase(generateColorCss(options?.defaultFlavour, options?.prefix));
     };
   },
   (options) => {
     // generate the options mapping
-    const extendOption = {};
+    const extendOption: Partial<ThemeConfig> = {};
     colourConfigKeys.map((key) => {
-      extendOption[key] = generateOptions(options?.prefix);
+      extendOption[key] = generateOptions(options.prefix);
     });
 
-    return {
+    const config: Config = {
+      content: [],
       theme: {
         extend: extendOption,
       },
     };
+
+    return config;
   }
 );
