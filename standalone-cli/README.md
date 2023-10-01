a42cc603eab39d57b9f42cf9bf82006522291b32
jolheiser <john.olheiser@gmail.com>
2023-09-30 21:39:11 -0500

    feat: standalone cli
    Signed-off-by: jolheiser <john.olheiser@gmail.com>

---
+++ standalone-cli/README.md
@@ -0,0 +1,22 @@
+# Tailwind Standalone CLI
+
+> Standalone version of [Tailwind CLI](https://tailwindcss.com/docs/installation#using-tailwind-cli) + [Catppuccin](https://github.com/catppuccin/tailwindcss)
+
+## Installation
+
+Download the appropriate platform-specific version of the `tailwindctp` cli from the [latest release](https://github.com/catppuccin/tailwindcss/releases).
+
+## Usage
+
+```
+./tailwindctp-macos -o tailwind.css
+```
+
+Check out the [Tailwind CLI documentation](https://tailwindcss.com/docs/installation#using-tailwind-cli) for details on the available options.
+
+
+## License
+
+[MIT](../LICENSE)
+
+This CLI is copy-pasted, credit to the [original license](https://github.com/tailwindlabs/tailwindcss/blob/5542340a75d23396b7c9dca462e2958728415b2d/LICENSE)
---
+++ standalone-cli/standalone.js
@@ -0,0 +1,38 @@
+let Module = require("module");
+let origRequire = Module.prototype.require;
+let log = require("tailwindcss/lib/util/log").default;
+
+let localModules = {
+  "tailwindcss/colors": require("tailwindcss/colors"),
+  "tailwindcss/defaultConfig": require("tailwindcss/defaultConfig"),
+  "tailwindcss/defaultTheme": require("tailwindcss/defaultTheme"),
+  "tailwindcss/resolveConfig": require("tailwindcss/resolveConfig"),
+  "tailwindcss/plugin": require("tailwindcss/plugin"),
+
+  "@tailwindcss/aspect-ratio": require("@tailwindcss/aspect-ratio"),
+  "@tailwindcss/container-queries": require("@tailwindcss/container-queries"),
+  "@tailwindcss/forms": require("@tailwindcss/forms"),
+  "@tailwindcss/line-clamp": () => {
+    log.warn("line-clamp-in-core", [
+      "As of Tailwind CSS v3.3, the `@tailwindcss/line-clamp` plugin is now included by default.",
+      "Remove it from the `plugins` array in your configuration to eliminate this warning.",
+    ]);
+  },
+  "@tailwindcss/typography": require("@tailwindcss/typography"),
+
+  // These are present to allow them to be specified in the PostCSS config file
+  autoprefixer: require("autoprefixer"),
+  tailwindcss: require("tailwindcss"),
+
+  // Catppuccin
+  "@catppuccin/tailwindcss": require("@catppuccin/tailwindcss"),
+};
+
+Module.prototype.require = function (id) {
+  if (localModules.hasOwnProperty(id)) {
+    return localModules[id];
+  }
+  return origRequire.apply(this, arguments);
+};
+
+require("tailwindcss/lib/cli");
